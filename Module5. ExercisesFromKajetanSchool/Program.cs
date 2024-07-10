1cc660a331a78b4a9c49cd38e80b9229155dc23b
Ilka <Ilka@LAPTOP-VAVOB6QC>
2024-07-10 17:56:03 +0200

    Add project files.

---
+++ Module5. ExercisesFromKajetanSchool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module5._ExercisesFromKajetanSchool_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, moduł 5");
+        }
+    }
+}
